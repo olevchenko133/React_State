f97b505cb6f7c7cb461e1a8e854d219eebbd8884
Olga <o.levchenko133@gmail.com>
2020-06-13 14:19:09 +0300

    all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // COSTRUCTOR
+  constructor(props) {
+    super(props);
+    this.state = {
+      numClicks: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+let num = Math.floor(Math.random() * 10) +1;
+    this.setState({ numClicks: num });
+
+  }
+
+
+  render() {
+    return (
+<div className="App"> 
+  <h1>Number is: {this.state.numClicks} </h1>
+
+  { this.state.numClicks ===7 ? <h2>You win</h2>
+ : <button onClick={this.handleClick} >Generate number </button> }
+
+</div>
+    )
+  }
 }
+
+
+
 
 export default App;
